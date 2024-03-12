3910b08249fadded99dcd9610a504736ef630bdf
Ganesh Kumar <mrganeshofficial8@gmail.com>
2024-03-12 17:54:30 +0530

    tokenomics components responsivness added

--- saviour/src/Components/Tokenomics.jsx
+++ saviour/src/Components/Tokenomics.jsx
@@ -6,8 +6,8 @@ export const Tokenomics = () =>{
     return <>
    <main className="pb-10 pt-10" style={{backgroundImage: `url(${backgroundImg})`,backgroundSize: 'cover', backgroundPosition: 'center'}} >
     <p className="text-red-600 text-3xl font-shojumaru p-10">Tokenomics</p>
-    <div className="flex justify-around items-center ">
-        <div className=" relative border border-red-600 w-1/4   rounded-2xl text-center pt-10 pb-8 ">
+    <div className="flex  flex-col md:flex-row justify-around items-center mt-10 ">
+        <div className=" relative border border-red-600 w-full md:w-1/4   rounded-2xl text-center pt-10 pb-8 ">
             <table className="  text-white font-ZCOOLKuaiLe  text-center  p-3 w-full" >
                 <tbody >
                 <tr >
@@ -33,7 +33,7 @@ export const Tokenomics = () =>{
                 <p>Token Details</p>
             </div>
         </div>
-        <div className=" relative w-1/2">
+        <div className=" relative w-full md:w-1/2 mt-[50px]">
             <img src={tokenomicsLogo} alt="Logo" className="" />
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/3 w-1/3"><img src={logo} alt="logo" className=" w-full object-cover" /></div>
         </div>
